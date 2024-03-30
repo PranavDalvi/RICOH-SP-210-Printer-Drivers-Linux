5ac201dbef175e0ba8d30217787038e688c50ea3
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-03-30 19:42:49 +0530

    changes to script file

--- install-ricoh-drivers.sh
+++ install-ricoh-drivers.sh
@@ -4,10 +4,14 @@ FilterPath="/usr/lib/cups/filter"
 rasterName="rastertolilo"
 ppdName="RICOH-SP-210.ppd"
 ppdPath="/etc/cups/ppd"
+modeldir="ricoh"
+cupsModelPath="/usr/share/cups/model"
 
 cp ./$rasterName $FilterPath
 
 cp ./$ppdName $ppdPath
+
+cp -r ./$modeldir $cupsModelPath
 
 chown root:root $FilterPath/$rasterName
 
